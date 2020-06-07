aa7151d561cb8a48e66916b4ded1742e095393f9
awlin <awlin@berkeley.edu>
2020-06-07 14:26:56 -0700

    president-message

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 var typed = new Typed('#typed', {
-  strings: ['CREATIVE.', 'PROFESSIONAL.', 'A BROTHERHOOD.', 'PSE.'],
+  strings: ['CREATIVE.', 'PROFESSIONAL.', 'A BROTHERHOOD.', 'PI SIGMA EPSILON.'],
   smartBackspace: true,
   typeSpeed: 80,
   startDelay: 500,
