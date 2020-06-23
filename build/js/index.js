f22460f00a87ee65e02e3a860e17f9ad4b54c518
Amit Palekar <amit.palekar@berkeley.edu>
2020-06-22 20:27:29 -0700

    Add build folder

---
+++ build/js/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    var typed = new Typed('#typed', {
+      strings: ['CREATIVE.', 'PROFESSIONAL.', 'A BROTHERHOOD.', 'PI SIGMA EPSILON.'],
+      smartBackspace: true,
+      typeSpeed: 80,
+      startDelay: 500,
+      backDelay: 700
+    });
+});
