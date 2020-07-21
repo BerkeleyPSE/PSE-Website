81b052ac89e24bec36dfa1e08a80b03b81ab341e
Amit Palekar <amit.palekar@berkeley.edu>
2020-07-20 18:24:37 -0700

    Remove build dependencies

--- build/js/index.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    var typed = new Typed('#typed', {
-      strings: ['CREATIVE.', 'PROFESSIONAL.', 'A BROTHERHOOD.', 'PI SIGMA EPSILON.'],
-      smartBackspace: true,
-      typeSpeed: 80,
-      startDelay: 500,
-      backDelay: 700
-    });
-});
