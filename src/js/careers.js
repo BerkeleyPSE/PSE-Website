756197a2fa1e218ce79200dbb7393672d89769d9
Amit Palekar <amit.palekar@berkeley.edu>
2020-07-25 20:28:08 -0700

    Fix brothers

---
+++ src/js/careers.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    let MONGO_URI=mongodb://amit_palekar:pisigmaepsilon1!@ds113736.mlab.com:13736/pse-website-dev
+    let GOOGLE_CLIENT_ID=758043936937-3mbspoec3tmkjidrf07hvcgsb5p7asb5.apps.googleusercontent.com
+    let GOOGLE_CLIENT_SECRET=txn0uMhiS0Jzx-7CWFSiE5hQ
+    getCareersData()
+    $('#content-table').DataTable()
+});
+
+function getCareersData() {
+
+}
