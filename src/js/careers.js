017e0a93d847805c604388182834d75f8d351efc
Amit Palekar <amit.palekar@berkeley.edu>
2020-08-03 18:47:18 -0700

    Add careers page

--- src/js/careers.js
+++ src/js/careers.js
@@ -1,11 +1,22 @@
 $(document).ready(function() {
-    let MONGO_URI=mongodb://amit_palekar:pisigmaepsilon1!@ds113736.mlab.com:13736/pse-website-dev
-    let GOOGLE_CLIENT_ID=758043936937-3mbspoec3tmkjidrf07hvcgsb5p7asb5.apps.googleusercontent.com
-    let GOOGLE_CLIENT_SECRET=txn0uMhiS0Jzx-7CWFSiE5hQ
-    getCareersData()
-    $('#content-table').DataTable()
+    let sheetID = "1O25K6kRSB93Q8uXVfPF57MyrtEphdRS9XWVkM6js540"
+    let prefix = "https://spreadsheets.google.com/feeds/list/"
+    let postfix = "/od6/public/values?alt=json"
+    let spreadsheetURL = prefix + sheetID + postfix
+    people = []
+    $.getJSON(spreadsheetURL, function(data) {
+        console.log(data)
+        // console.log(data.feed.entry);
+        // console.log("---------")
+        for (d of data.feed.entry) {
+            people.push([d.gsx$name.$t, d.gsx$position.$t, d.gsx$category.$t, d.gsx$location.$t])
+        }
+        // console.log("---------")
+        console.log(people)
+        $('#content-table').DataTable({
+            data: people
+        })
+    })
+    console.log("hello")
+
 });
-
-function getCareersData() {
-
-}
